73a2dfe316a050cb4cb7b4303cfcabac3a59877e
Arturo Quiros <adquirose@gmail.com>
2022-09-12 13:47:19 -0300

    arreglo rutas reset-password

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = 'https://strapi-rocp.onrender.com';
+export const BACKEND_URL = 'http://localhost:1337';
+// export const BACKEND_URL = 'https://strapi-rocp.onrender.com';
 export const FacebookBackground =
   'linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)';
 export const InstagramBackground =
